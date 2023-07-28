6d6a93b4cf79bddb9cce27a4ddb4ad00931c1fc0
jayant-1998 <jayant.grover@remotestate.com>
2023-07-28 18:39:13 +0530

    demo commit

--- DAL/Entity/TodoItem.cs
+++ DAL/Entity/TodoItem.cs
@@ -9,6 +9,7 @@ namespace TodoAPI.DAL.Entity
     {
         [Key]
         public int ID { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
